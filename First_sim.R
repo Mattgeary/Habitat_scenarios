486d3150536105ea25d7d7e38bd409136ba42b83
Matt Geary <mattg533@gmail.com>
2014-03-03 11:50:01 +0000

    Revert "Results of example simulations"
    This reverts commit c1eb3e69428b041a565606d81dfaeb354db2dea8.

--- First_sim.R
+++ First_sim.R
@@ -5,13 +5,6 @@ output.6 <- paste(getwd(), "Scenario_6", sep="/")
 
 BNG <- CRS("+init=epsg:27700")
 
-models <- 10
-#ind <- rep(1, 10)
-iterations <- 50
-p.trans <- 0.25
-n.points <- 20
-
-
 f.prob <- function(x, p.trans){
           x*p.trans
  }
@@ -26,7 +19,7 @@ f.correct <- function(x){
   else x
 }
 
-### Function to grow new habitat
+### Needs fixing, currently changes NAs
 
 f.Exp <- function(map, base.map, p.trans){
               mat.0 <- as.matrix(map)
@@ -46,21 +39,10 @@ BK.pres <- read.csv("BK_pres.csv")
 
 env.l.base <- stack(raster(paste(getwd(), "Proportion/hab_1.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_2.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_3.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_4.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_5.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_6.asc", sep="/"), proj4string=BNG), raster("studyareadem.asc", proj4string=BNG))
 
-writeRaster(env.l.base, filename=(paste(getwd(), "Scenario_6/base_env.grd", sep="/")))
-
-#max_base <- maxent(env.l.base, BK.pres, args=c("replicates=10", "replicatetype=crossvalidate", "outputgrids=FALSE"))
-max_base <- maxent(env.l.base, BK.pres) 
-
+max_base <- maxent(env.l.base, BK.pres, args=c("replicates=10", "replicatetype=crossvalidate", "outputgrids=FALSE"))
 
 base.map <- predict(max_base, env.l.base)
-writeRaster(base.map, filename=(paste(getwd(), "Scenario_6/base_map.grd", sep="/")))
 
-#stackApply(base.map, ind, mean, filename=paste(getwd(), "Scenario_6/mean.base.map", sep="/"), format="ascii")
-
-rm(base.map)
-rm(env.l.base)
-
-for(j in 1:models){
 # Read in potential area layer
 
 potential <- raster(paste(getwd(), "Potential/Scenario_6.asc", sep="/"),  proj4string="BNG")
@@ -68,7 +50,7 @@ potential <- raster(paste(getwd(), "Potential/Scenario_6.asc", sep="/"),  proj4s
 rcl <- matrix(c(0.8,1.2,0), nrow=1, ncol=3, byrow=T)
 potential.0 <- reclass(potential, rcl)
 
-rnd.pts <- randomPoints(potential, n.points)
+rnd.pts <- randomPoints(potential, 10)
 rnd.pts <- as.data.frame(rnd.pts)
 potential.pts <- rasterize(rnd.pts, potential, background=0)
 potential.pts <- potential.0 + potential.pts
@@ -79,52 +61,44 @@ rm(potential)
 
 iterations <- 50
 p.trans <- 0.25
-new.hab <- f.Exp(potential.pts, potential.pts, p.trans)
+new.hab <- f.Env(potential.pts, potential.pts, p.trans)
 for(i in 1:iterations){
 	if(cellStats(new.hab, sum) <= 1848731) new.hab <- f.Exp(new.hab, potential.pts, p.trans)
 	else new.hab <- new.hab
-    }
+}
 
-dir.create(paste(getwd(), "/Scenario_6/run_", j, sep=""))
-
-hab.3 <- raster(paste(getwd(), "Binary/bin_3.asc", sep="/"))
+hab.3 <- raster(paste(getwd(), "Binary/hab_3.asc", sep="/"))
 hab.3 <- hab.3 - new.hab
 projection(hab.3) <- BNG
 hab.3 <- focal(hab.3, w=93, sum, na.rm=T, pad=T)
 setwd(output.6)
-writeRaster(hab.3, paste(paste(getwd(), "/run_", j, sep=""), "/hab_3.asc", sep=""), overwrite=T)
+writeRaster(hab.3, "hab_3.asc")
 rm(hab.3)
 setwd(work)
 
-hab.4 <- raster(paste(getwd(), "Binary/bin_4.asc", sep="/"))
+hab.4 <- raster(paste(getwd(), "Binary/hab_4.asc", sep="/"))
 hab.4 <- hab.4 - new.hab
 projection(hab.4) <- BNG
 hab.4 <- focal(hab.4, w=93, sum, na.rm=T, pad=T)
 setwd(output.6)
-writeRaster(hab.4, paste(paste(getwd(), "/run_", j, sep=""), "/hab_4.asc", sep=""), overwrite=T)
+writeRaster(hab.4, "hab_4.asc")
 rm(hab.4)
 setwd(work)
 
-hab.5 <- raster(paste(getwd(), "Binary/bin_5.asc", sep="/"))
-hab.5 <- hab.5 + new.hab
+hab.5 <- raster(paste(getwd(), "Binary", sep="/"))
+hab.3 <- hab.5 + new.hab
 projection(hab.5) <- BNG
-hab.5 <- focal(hab.5, w=93, sum, na.rm=T, pad=T)
+hab.3 <- focal(hab.5, w=93, sum, na.rm=T, pad=T)
 setwd(output.6)
-writeRaster(hab.5, paste(paste(getwd(), "/run_", j, sep=""), "/hab_5.asc", sep=""), overwrite=T)
+writeRaster(hab.3, "hab_4.asc")
 rm(hab.5)
 setwd(work)
 
-gc()
-env.l.new <- stack(raster(paste(getwd(), "Proportion/hab_1.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_2.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), paste("Scenario_6/run_", j, sep=""), "hab_3.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), paste("Scenario_6/run_", j, sep=""), "hab_4.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), paste("Scenario_6/run_", j, sep=""), "hab_5.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_6.asc", sep="/"), proj4string=BNG), raster("studyareadem.asc", proj4string=BNG))
+env.l.new <- stack(raster(paste(getwd(), "Proportion/hab_1.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_2.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Scenario_6/hab_3.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Scenario_6/hab_4.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Scenario_6/hab_5.asc", sep="/"), proj4string=BNG), raster(paste(getwd(), "Proportion/hab_6.asc", sep="/"), proj4string=BNG), raster("studyareadem.asc", proj4string=BNG))
 
 pred <- predict(max_base, env.l.new)
-rm(env.l.new)
-gc()
-writeRaster(pred, filename=(paste(getwd(), "Scenario_6", paste("pred_map", j, ".grd", sep= ""), sep="/")))
 
-#stackApply(pred, ind, mean, na.rm=T, filename=paste(getwd(), "Scenario_6", paste("new_", j, ".asc", sep=""), sep="/"))
-rm(pred)
-#writeRaster(pred, paste("Scenario_6/Models/Pred_MaxEnt_", j, ".asc", sep=""))
+ind <- rep(1, 10)
 
-}
-
+pred <- stackApply(pred, ind, mean, na.rm=T)
+writeRaster(pred, "Pred_MaxEnt.asc")
